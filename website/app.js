625d919fe88d3c9f36f26c9739d2812ac5515885
Alison Lee <alison2686@gmail.com>
2021-08-26 15:43:07 -0700

    retrieve weather data

--- website/app.js
+++ website/app.js
@@ -1,19 +1,40 @@
-  
-// Personal API Key for OpenWeatherMap API
-
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
-
-/* Function to GET Web API Data*/
-
 /* Function to POST data */
 
 
 /* Function to GET Project Data */
 
 /* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+// Personal API Key for OpenWeatherMap API
+let apiKey = '&appid=38dc2cc844dff231254225ded0287cc9&units=metric';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', getWeather);
+
+/* Function called by event listener */
+function getWeather(event) {
+    let zip = document.getElementById('zip').value;
+    getWeatherData(baseURL, zip, apiKey);
+}
+
+/* Function to GET Web API Data*/
+const getWeatherData = async (baseURL, zip, key) => {
+    const res = await fetch(baseURL+zip+key);
+    console.log(res);
+    try {
+        const data = await res.json();
+        console.log(data);
+        // chain promise here
+
+        // postData('/weather', data)
+    } catch(error) {
+        console.log('error', error);
+    }
+}
+
+
+
